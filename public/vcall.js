e15581009c6584f8b6685a4bac968de712f23ab0
WilliamDann <wcdann1@gmail.com>
2020-04-17 11:00:07 -0700

    added public

---
+++ public/vcall.js
@@ -0,0 +1,53 @@
+var server = new WebSocket("ws://localhost:8080");
+var stream = new WebSocket("ws://localhost:8081");
+
+const display = document.querySelector('#serverdata');
+const video = document.querySelector('#video');
+
+var userid = null;
+
+// get webcam access
+navigator.mediaDevices.getUserMedia({video: {width: 360, height: 360}}).then((stream) => video.srcObject = stream);
+
+function getFrame() {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    const data = canvas.toDataURL('image/jpeg');
+
+    return data;
+}
+
+// send data
+server.onopen = () => {
+    console.log("connected to call.");
+}
+
+var next = false
+server.onmessage = message => {
+
+    if (next == 'uid') {
+        if (!userid) {
+            userid = message;
+            next = false;
+
+            // start streaming
+            setInterval(() => {
+                stream.send( JSON.stringify({ userID: userid, frame: getFrame() }) );
+            }, 100) // todo fps?
+        } else {
+            userid = message;
+            next = false;
+        }
+    }
+
+    if (message == 'userdata') {
+        // todo get userdata from user
+        server.send(JSON.stringify({ username: "TestUser", isBroadcaster: false }));
+    }
+
+    if (message == "set userID") {
+        next = 'uid';
+    }
+}
